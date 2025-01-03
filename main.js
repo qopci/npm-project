0ad9727b76cbd982ee61d4e80231d5cfae84c601
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-01-03 14:30:50 -0800

    Finished up Lab #2

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import chalk from 'chalk';
 // INSTALL: npm i lodash
 const array = [1, 4, 2, 3, 1, 4, 5, 2, 3, 2, 1];
 const uniqueNums = lodash.uniq(array);
+console.log(chalk.red(array));
 console.log(uniqueNums);
 
 // Chalk Package Usage (Terminal String Color Picker)
